eaea21557858b6c80a40b373c2d197f2534fa71b
Tarun-msrit <tarun.ragothaman@gmail.com>
2021-09-01 12:30:49 +0530

    commit "fruit added"

--- TestProject/src/main/java/com/testing/controller/TestController.java
+++ TestProject/src/main/java/com/testing/controller/TestController.java
@@ -14,17 +14,11 @@ public class TestController {
 	}
 	
 
-
 	@GetMapping("/fruit")
 	public String getFruit() {
 		return "Mango";
 	}
 	
-	@GetMapping("/bye")
-	public String getBye() {
-		return "Good Bye Tarun";
-	}
-
 
 	@GetMapping("/welcome")
 	public String getWelcome() {
