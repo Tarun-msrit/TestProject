1f5986b53058d7a80afe7045078388c61a178ab8
Tarun-msrit <tarun.ragothaman@gmail.com>
2021-09-01 11:42:18 +0530

    commit fruit

--- TestProject/src/main/java/com/testing/controller/TestController.java
+++ TestProject/src/main/java/com/testing/controller/TestController.java
@@ -18,6 +18,12 @@ public class TestController {
 		return "Hello Tarun";
 	}
 	
+	@GetMapping("/fruit")
+	public String getFruit() {
+		return "Mango";
+	}
+	
+	
 	@GetMapping("/color")
 	public String getColor() {
 		return "Its Orange";
